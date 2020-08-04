af8db481cc57d68b15a0432e781773a88b853bcf
Alexys Santiago <SuxPorT@hotmail.com>
2020-08-03 22:08:20 -0300

    Refactoring code

--- Main.py
+++ Main.py
@@ -1,17 +1,22 @@
-import WhatsappClass
+from WhatsappClass import WhatsappBot
 
-if __name__ == '__main__':
+
+def main():
     message = str(input("Mensagem a ser enviada: "))
     groups = []
     loop = True
 
-    while (loop):
-        toGroup = str(input("Grupo de destino: "))
+    while loop:
+        to_group = str(input("Grupo de destino (enter para finalizar): "))
 
-        if not toGroup:
+        if not to_group:
             loop = False
         else:
             groups.append(toGroup)
 
-    bot = WhatsappClass.WhatsappBot(message, groups)
-    bot.SendMessages()
+    bot = WhatsappBot(message, groups)
+    bot.send_messages()
+
+
+if __name__ == '__main__':
+    main()
