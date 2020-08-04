7fd42e1aea294a4780bfb83c6f5f72561b6e0bf1
Alexys Santiago <SuxPorT@hotmail.com>
2020-08-03 22:05:21 -0300

    Refactoring code

--- WhatsappClass.py
+++ WhatsappClass.py
@@ -1,7 +1,9 @@
 from selenium import webdriver
 import time
 
+
 class WhatsappBot():
+
     def __init__(self, message, groups):
         """ Construtor da classe
         
@@ -13,15 +15,17 @@ class WhatsappBot():
         self.groups = groups
         options = webdriver.ChromeOptions()
         options.add_argument("lang=pt-br")
-        self.driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
+        self.driver = webdriver.Chrome(executable_path=r"./chromedriver.exe")
 
-    def SendMessages(self):
+    def send_messages(self):
         """ Função que irá acionar as mensagens, sendo composta de três elementos HTML:
         1. Span do título do grupo
         2. Div da caixa de texto da mensagem
         3. Span do botão de envio da mensagem
         """
-        self.driver.get('https://web.whatsapp.com/')
+        self.driver.get("https://web.whatsapp.com/")
+
+        # Espera para fazer a autenticação
         time.sleep(30)
 
         for group in self.groups:
@@ -31,7 +35,7 @@ class WhatsappBot():
             group.click()
 
             # Div do HTML indicando a caixa de texto da mensagem
-            chat_box = self.driver.find_element_by_class_name('_3uMse')
+            chat_box = self.driver.find_element_by_class_name("_3uMse")
             time.sleep(3)
             chat_box.click()
             chat_box.send_keys(self.message)
@@ -40,5 +44,4 @@ class WhatsappBot():
             send_button = self.driver.find_element_by_xpath("//span[@data-icon='send']")
             time.sleep(3)
             send_button.click()
-
             time.sleep(5)
